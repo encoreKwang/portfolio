3f9796df902b50f4b20a14257c7b1bc2a4bc1dd8
kwangyong <dnflrhkddyd@naver.com>
2022-01-14 13:11:19 +0900

    JS : navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ document.addEventListener('scroll', () => {
     }
 })
 
+
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click' ,(event) => {
@@ -21,8 +22,16 @@ navbarMenu.addEventListener('click' ,(event) => {
     if(link == null){
         return;
     }
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 })
+
+//navbar toggle button for small screen
+const navToggle = document.querySelector('.navbar__toggle-btn');
+navToggle.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+})
+
 
 //Handle click on "contact me" button on home
 const btnContact = document.querySelector('.home__contact');
@@ -85,6 +94,8 @@ workBtnContainer.addEventListener('click', (event) => {
     }, 300)
     
 })
+
+
 
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
